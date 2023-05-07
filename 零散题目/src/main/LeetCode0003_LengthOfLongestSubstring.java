dbcc677e1fbe4be0d7d62944bc2a76009f13731a
祝浪 <zhulang013@hellobike.com>
2023-05-07 21:22:13 +0800

    股票交易问题

--- 零散题目/src/main/LeetCode0003_LengthOfLongestSubstring.java
+++ 零散题目/src/main/LeetCode0003_LengthOfLongestSubstring.java
@@ -8,7 +8,7 @@ import java.util.Map;
  * @Date 2023-04-26
  **/
 public class LeetCode0003_LengthOfLongestSubstring {
-    static class Solution{
+    static class Solution {
         public static void main(String[] args) {
             String s = "abba";
             System.out.println(lengthOfLongestSubstring(s));
@@ -20,15 +20,15 @@ public class LeetCode0003_LengthOfLongestSubstring {
             int R = 0;
             int max = 0;
             Map<Character, Integer> map = new HashMap<>();
-            while (R < str.length){
-                if (!map.containsKey(str[R])){
+            while (R < str.length) {
+                if (!map.containsKey(str[R])) {
                     map.put(str[R], R);
                     max = Math.max(R - L + 1, max);
                     R++;
                     continue;
                 }
                 int i = map.get(str[R]);
-                while (L <= i){
+                while (L <= i) {
                     map.remove(str[L]);
                     L++;
                 }
@@ -36,6 +36,26 @@ public class LeetCode0003_LengthOfLongestSubstring {
                 R++;
             }
             return max;
+        }
+
+        public static int lengthOfLongestSubstring2(String s) {
+            HashMap<Character, Integer> window = new HashMap<>();
+            int left = 0;
+            int right = 0;
+            int res = 0;
+            char[] str = s.toCharArray();
+            while (right < str.length) {
+                char c = str[right];
+                right++;
+                window.put(c, window.getOrDefault(c, 0) + 1);
+                while (window.get(c) > 1) {
+                    char d = str[left];
+                    left++;
+                    window.put(d, window.get(d) - 1);
+                }
+                res = Math.max(res, right - left);
+            }
+            return res;
         }
     }
 }
---
+++ 零散题目/src/main/LeetCode0123_MaxProfit.java
@@ -0,0 +1,38 @@
+package main;
+
+/**
+ * @Author zhulang
+ * @Date 2023-05-07
+ **/
+public class LeetCode0123_MaxProfit {
+    static class Solution {
+        public int maxProfit(int[] prices) {
+            int n = prices.length;
+            int[][][] dp = new int[n][3][2];
+            for (int i = 0; i < n; i++){
+                for (int k = 2; k >= 1; k--){
+                    if (i - 1 == -1) {
+                        dp[i][k][0] = 0;
+                        dp[i][k][1] = -prices[i];
+                        continue;
+                    }
+                    dp[i][k][0] = Math.max(dp[i - 1][k][0], dp[i - 1][k][1] + prices[i]);
+                    dp[i][k][1] = Math.max(dp[i - 1][k][1], dp[i - 1][k - 1][0] - prices[i]);
+                }
+            }
+            return dp[n - 1][2][0];
+        }
+
+        public int maxProfit2(int[] prices) {
+            int dp_i10 = 0, dp_i11 = Integer.MIN_VALUE;
+            int dp_i20 = 0, dp_i21 = Integer.MIN_VALUE;
+            for (int price : prices) {
+                dp_i20 = Math.max(dp_i20, dp_i21 + price);
+                dp_i21 = Math.max(dp_i21, dp_i10 - price);
+                dp_i10 = Math.max(dp_i10, dp_i11 + price);
+                dp_i11 = Math.max(dp_i11, -price);
+            }
+            return dp_i20;
+        }
+    }
+}
