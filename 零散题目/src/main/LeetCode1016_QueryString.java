fddb028d6b8833095ca56f484b4491d9d8df91bb
祝浪 <zhulang013@hellobike.com>
2023-05-11 14:02:56 +0800

    ten_to_binary

--- 零散题目/src/main/LeetCode1016_QueryString.java
+++ 零散题目/src/main/LeetCode1016_QueryString.java
@@ -8,19 +8,30 @@ public class LeetCode1016_QueryString {
 
     // O(N)
     public boolean queryString(String s, int n) {
-        for (int i = 1; i <= n; i++){
-            // 怎么自己转换二进制？
-            String binaryString = Integer.toBinaryString(i);
-            if (getIndexOf(s, binaryString) == -1){
+        for (int i = 1; i <= n; i++) {
+//            String binaryString = Integer.toBinaryString(i);
+            String binaryString = tenToBinary(i);
+            if (getIndexOf(s, binaryString) == -1) {
+
                 return false;
             }
         }
         return true;
     }
 
+    // 正整数十进制转二进制
+    public String tenToBinary(int n) {
+        StringBuilder s = new StringBuilder();
+        while (n > 0) {
+            s.insert(0, n % 2);
+            n = n / 2;
+        }
+        return s.toString();
+    }
+
     // O(M+N)
-    public static int getIndexOf(String s, String m){
-        if (s == null || m == null || s.length() < m.length() || m.length() < 1){
+    public static int getIndexOf(String s, String m) {
+        if (s == null || m == null || s.length() < m.length() || m.length() < 1) {
             return -1;
         }
 
@@ -29,11 +40,11 @@ public class LeetCode1016_QueryString {
         int[] next = getNextArray(str2);
         int l1 = 0;
         int l2 = 0;
-        while (l1 < str1.length && l2 < str2.length){
-            if (str1[l1] == str2[l2]){
+        while (l1 < str1.length && l2 < str2.length) {
+            if (str1[l1] == str2[l2]) {
                 l1++;
                 l2++;
-            } else if (next[l2] == -1){
+            } else if (next[l2] == -1) {
                 l1++;
             } else {
                 l2 = next[l2];
@@ -44,8 +55,8 @@ public class LeetCode1016_QueryString {
     }
 
 
-    public static int[] getNextArray(char[] ms){
-        if (ms.length == 1){
+    public static int[] getNextArray(char[] ms) {
+        if (ms.length == 1) {
             return new int[]{-1};
         }
 
@@ -54,10 +65,10 @@ public class LeetCode1016_QueryString {
         next[1] = 0;
         int cn = 0;
         int i = 2;
-        while (i < next.length){
-            if (ms[i - 1] == ms[cn]){
+        while (i < next.length) {
+            if (ms[i - 1] == ms[cn]) {
                 next[i++] = ++cn;
-            } else if (cn > 0){
+            } else if (cn > 0) {
                 cn = next[cn];
             } else {
                 next[i++] = 0;
