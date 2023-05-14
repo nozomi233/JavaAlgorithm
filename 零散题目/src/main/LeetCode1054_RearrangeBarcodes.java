05866b67482d5daab741edbf97ecc2b1b86c69ae
祝浪 <zhulang013@hellobike.com>
2023-05-14 09:27:14 +0800

    2023-05-14

---
+++ 零散题目/src/main/LeetCode1054_RearrangeBarcodes.java
@@ -0,0 +1,44 @@
+package main;
+
+import java.util.*;
+
+/**
+ * @Author zhulang
+ * @Date 2023-05-14
+ **/
+public class LeetCode1054_RearrangeBarcodes {
+    public int[] rearrangeBarcodes(int[] barcodes) {
+        Map<Integer, Integer> count = new HashMap<>();
+        for (int b : barcodes) {
+            if (!count.containsKey(b)) {
+                count.put(b, 0);
+            }
+            count.put(b, count.get(b) + 1);
+        }
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> b[0] - a[0]);
+        for (Map.Entry<Integer, Integer> entry : count.entrySet()) {
+            pq.offer(new int[]{entry.getValue(), entry.getKey()});
+        }
+        int n = barcodes.length;
+        int[] res = new int[n];
+        for (int i = 0; i < n; ++i) {
+            int[] p = pq.poll();
+            int cx = p[0], x = p[1];
+            if (i == 0 || res[i - 1] != x) {
+                res[i] = x;
+                if (cx > 1) {
+                    pq.offer(new int[]{cx - 1, x});
+                }
+            } else {
+                int[] p2 = pq.poll();
+                int cy = p2[0], y = p2[1];
+                res[i] = y;
+                if (cy > 1) {
+                    pq.offer(new int[]{cy - 1, y});
+                }
+                pq.offer(new int[]{cx, x});
+            }
+        }
+        return res;
+    }
+}
