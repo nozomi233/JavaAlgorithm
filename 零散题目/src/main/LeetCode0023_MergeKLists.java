c11869e9bf430d0b82d9cf8d8b9e805099377f54
祝浪 <zhulang013@hellobike.com>
2023-05-18 14:04:19 +0800

    mergeKLists

---
+++ 零散题目/src/main/LeetCode0023_MergeKLists.java
@@ -0,0 +1,36 @@
+package main;
+
+/**
+ * @Author zhulang
+ * @Date 2023-05-18
+ **/
+public class LeetCode0023_MergeKLists {
+    public ListNode mergeKLists(ListNode[] lists) {
+        ListNode ans = null;
+        for (int i = 0; i < lists.length; ++i) {
+            ans = mergeTwoLists(ans, lists[i]);
+        }
+        return ans;
+    }
+
+    public ListNode mergeTwoLists(ListNode a, ListNode b) {
+        if (a == null || b == null) {
+            return a != null ? a : b;
+        }
+        ListNode head = new ListNode(0);
+        ListNode tail = head, aPtr = a, bPtr = b;
+        while (aPtr != null && bPtr != null) {
+            if (aPtr.val < bPtr.val) {
+                tail.next = aPtr;
+                aPtr = aPtr.next;
+            } else {
+                tail.next = bPtr;
+                bPtr = bPtr.next;
+            }
+            tail = tail.next;
+        }
+        tail.next = (aPtr != null ? aPtr : bPtr);
+        return head.next;
+    }
+
+}
