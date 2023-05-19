9f1e0da012ad77e9600d2766efbccd63ccbc988f
祝浪 <zhulang013@hellobike.com>
2023-05-19 17:20:59 +0800

    2023-05-19

---
+++ 零散题目/src/main/LeetCode0032_LongestValidParentheses.java
@@ -0,0 +1,29 @@
+package main;
+
+import java.util.Map;
+
+/**
+ * @Author zhulang
+ * @Date 2023-05-19
+ **/
+public class LeetCode0032_LongestValidParentheses {
+    public static int longestValidParentheses(String s) {
+        int max = 0;
+        int[] dp = new int[s.length()];
+        for (int i = 1; i < s.length(); i++) {
+            if (s.charAt(i) == ')') {
+                if (s.charAt(i - 1) == '(') {
+                    dp[i] = (i >= 2 ? dp[i - 2] : 0) + 2;
+                } else if (i - dp[i - 1] > 0 && s.charAt(i - dp[i - 1] - 1) == '(') {
+                    dp[i] = dp[i - 1] + ((i - dp[i - 1]) >= 2 ? dp[i - dp[i - 1] - 2] : 0) + 2;
+                }
+                max = Math.max(max, dp[i]);
+            }
+        }
+        return max;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(longestValidParentheses("(()())"));
+    }
+}
