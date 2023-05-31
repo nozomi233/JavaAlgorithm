250c2db3036a6ccf3d6b87c7751bdaa77c3ae8af
祝浪 <zhulang013@hellobike.com>
2023-05-31 09:13:08 +0800

    2023-05-31

---
+++ 零散题目/src/main/leetcode/LeetCode1130_MctFromLeafValues.java
@@ -0,0 +1,29 @@
+package main.leetcode;
+
+import java.util.Arrays;
+
+/**
+ * @Author zhulang
+ * @Date 2023-05-31
+ **/
+public class LeetCode1130_MctFromLeafValues {
+    public int mctFromLeafValues(int[] arr) {
+        int n = arr.length;
+        int[][] dp = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            Arrays.fill(dp[i], Integer.MAX_VALUE / 4);
+        }
+        int[][] mval = new int[n][n];
+        for (int j = 0; j < n; j++) {
+            mval[j][j] = arr[j];
+            dp[j][j] = 0;
+            for (int i = j - 1; i >= 0; i--) {
+                mval[i][j] = Math.max(arr[i], mval[i + 1][j]);
+                for (int k = i; k < j; k++) {
+                    dp[i][j] = Math.min(dp[i][j], dp[i][k] + dp[k + 1][j] + mval[i][k] * mval[k + 1][j]);
+                }
+            }
+        }
+        return dp[0][n - 1];
+    }
+}
